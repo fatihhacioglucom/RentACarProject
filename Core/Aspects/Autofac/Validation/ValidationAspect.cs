e2bab86732c71dded44604b7a5a70969c94b770d
Fatih Hacıoğlu <fatihhacioglucom@gmail.com>
2022-03-09 00:04:42 +0300

    Caching added

--- Core/Aspects/Autofac/Validation/ValidationAspect.cs
+++ Core/Aspects/Autofac/Validation/ValidationAspect.cs
@@ -8,7 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Core.Aspects.Validation
+namespace Core.Aspects.Autofac.Validation
 {
     public class ValidationAspect : MethodInterception
     {
