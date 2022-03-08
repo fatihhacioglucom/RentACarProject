80d08c98a939a6e64e086c0689f0d64f5b3eccf2
Fatih Hacıoğlu <fatihhacioglucom@gmail.com>
2022-03-08 23:01:09 +0300

    Core Module added

--- DataAccess/Concrete/EntityFramework/Context/RentACarContext.cs
+++ DataAccess/Concrete/EntityFramework/Context/RentACarContext.cs
@@ -15,9 +15,7 @@ namespace DataAccess.Concrete.EntityFramework.Context
         {
             optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB; Database=RentACarDb; Trusted_Connection=true");
         }
-        public DbSet<OperationClaim> OperationClaims { get; set; }
-        public DbSet<User> Users { get; set; }
-        public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
+        
 
         public DbSet<Car> Cars { get; set; }
         public DbSet<Brand> Brands { get; set; }
@@ -25,5 +23,8 @@ namespace DataAccess.Concrete.EntityFramework.Context
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Rental> Rentals { get; set; }
         public DbSet<CarImage> CarImages { get; set; }
+        public DbSet<OperationClaim> OperationClaims { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
     }
 }
